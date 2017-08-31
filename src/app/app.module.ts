b72d568bf6e956a45e9a0eafe1c8588b096a2571
lmadhuranga <lilan.maduranga@gmail.com>
2017-08-31 15:52:29 +0000

    Added Angular material desing to ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms'
+import {AppComponent} from './app.component';
 
-import { UserComponent } from './components/user/user.component';
-import { AboutusComponent } from './components/aboutus/aboutus.component';
+import {UserComponent} from './components/user/user.component';
+import {AboutusComponent} from './components/aboutus/aboutus.component';
 
-import { HttpModule } from '@angular/http';
-import { RouterModule, Routes } from '@angular/router';
+import {HttpModule} from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MdButtonModule,
+  MdListModule,
+  MdToolbarModule,
+} from '@angular/material';
 
-const appRoutes:Routes =[
-  {path:'', component:UserComponent},
-  {path:'about', component:AboutusComponent},
+
+const appRoutes: Routes = [
+  {path: '', component: UserComponent},
+  {path: 'about', component: AboutusComponent},
 
 ]
 
-import { DataService } from './services/data.service';
+import {DataService} from './services/data.service';
 
 @NgModule({
   declarations: [
@@ -27,9 +34,21 @@ import { DataService } from './services/data.service';
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    // Material Design Imports--------------------|
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdListModule,
+    MdToolbarModule,
+    RouterModule.forRoot(appRoutes),
+    // Material Design Imports--------------------|
+  ],
+  exports: [
+    MdButtonModule,
+    MdListModule,
+    MdToolbarModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,69 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService} from '../../services/data.service'
+import {Component, OnInit} from '@angular/core';
+import {DataService} from '../../services/data.service';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
+
 export class UserComponent implements OnInit {
-  name:string;
-  email:string;
-  age:number;
-  address:Address
-  hobbies:any;
-  posts:PostInterface;
+  posts: PostInterface;
 
-  constructor(private dataService:DataService) { 
-    console.log('mad_msg__ constractur ran');
-
-  }
+  constructor(private dataService: DataService) {}
 
   ngOnInit() {
-    console.log('mad_msg__ on init ran');
-    this.name = 'madhuranga';
-    this.age = 27;
-    this.email = 'madhuranga@gasdflsd.lk';
-    this.hobbies = ['Cricket', 'Sing', 'Reading'];
-    this.address ={
-      street:'Breakwoodplace',
-      city:'colombo',
-      state:'combo'
-    }
-
-    this.dataService.getPosts().subscribe((posts)=>{
+    this.dataService.getPosts().subscribe((posts) => {
       this.posts = posts;
-    })
+    });
   }
-
-  deleteHobby(oldHobby:string){
-    for(let i =0; i <this.hobbies.length; i++){
-      if(this.hobbies[i]==oldHobby){
-        this.hobbies.splice(i,1);
-      }
-    }
-  }
-
-  onSubmit(newHobby:string){
-    this.hobbies.unshift(newHobby)
-  }
-
-  onClick(newname:string){
-    console.log('mad_msg__newname',newname);
-    
-    this.name = newname;
-  }
-
 }
 
-interface Address {
-  street : string,
-  city: string,
-  state:string
-}
-
-interface PostInterface{
-  id:number,
-  title:string,
-  body:string,
-  userId:number
+interface PostInterface {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
 }
